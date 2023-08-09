5f9f21743d09a252c26d656f9bcb0ff9dde8920f
jamesLawson1 <jaylaw100.jl@gmail.com>
2023-08-09 11:20:49 +0000

    React Components Pt 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
